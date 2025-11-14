e9181222cf275e51e5eea412a332d8fb3ec7d305
YuvasreeJagadeesan <yashravj.240421@cse.ritchennai.edu.in>
2025-11-14 22:41:31 +0530

    Add files via upload

---
+++ FitnessController.java
@@ -0,0 +1,13 @@
+package com.yash.sentiment.backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class FitnessController 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
